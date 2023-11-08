3b86b66faf06d4bf1c04360fa00aff2ced93a6e3
David <evil.dave59@gmail.com>
2023-11-08 14:27:35 +0100

    ajout dans la collection cart avec le book

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+function book() {
+  // déclenchement du click à chaque bouton book
+  let books = document.querySelectorAll(".purchase-book");
+  books.forEach((book) => {
+    book.addEventListener("click", () => {
+      const id = book.getAttribute("data-id");
+      // recherche du trajet ciblé grace à son id dans la collection trips
+      fetch("http://localhost:3000/trips/id", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ _id: id }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          // ajout de la data.trips dans la new cart
+          fetch("http://localhost:3000/cart", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data.trips),
+          })
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+        });
+    });
+  });
+}
+
 document.querySelector("#searching-btn").addEventListener("click", function () {
   const trip = {
     departure: document.querySelector("#searching-departure").value,
@@ -19,7 +46,6 @@ document.querySelector("#searching-btn").addEventListener("click", function () {
         document.querySelector("#searching-arrival").value = "";
         // ajouter les trajets au row-content à travers une boucle
         for (let i = 0; i < data.trips.length; i++) {
-          console.log(data.trips[i]._id);
           const hour = data.trips[i].date.slice(11, 16);
           document.querySelector("#row-content").innerHTML += `
         <div class="purchase-row">
@@ -28,6 +54,7 @@ document.querySelector("#searching-btn").addEventListener("click", function () {
             <p class="purchase-price">${data.trips[i].price}$</p>
             <button class="purchase-book" data-id="${data.trips[i]._id}">book</button>
           </div>`;
+          book();
         }
       });
   } else {
