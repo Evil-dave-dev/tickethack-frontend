0fb9023a171ac4e6d3b8d6de58f78ed7d87d6d63
JoM <jordan.marescaux@gmail.com>
2023-11-08 13:06:03 +0100

    cart total

--- script-cart.js
+++ script-cart.js
@@ -3,8 +3,20 @@ fetch('http://localhost:3000/carts', {
   })
   .then (response => response.json())
   .then (data => {
-    console.log(data)
     
+   function supTrip() {
+      let X = document.querySelectorAll('.delete');
+      for (let i = 0; i < X.length; i++) {
+        
+      X[i].addEventListener('click', function() {
+        this.parentNode.remove();
+        fetch(`http://localhost:3000/carts/deleteOne/${data[i]._id}`, {
+          method: "DELETE",
+    });
+      })
+    }
+    }
+    let totalCost = 0;
     for (let i = 0; i < data.length; i++) {
       document.querySelector('#purchase-container').innerHTML += `
       <div class="purchase-row">
@@ -13,21 +25,21 @@ fetch('http://localhost:3000/carts', {
           <p class="purchase-price">${data[i].price}$</p>
           <span class="delete">&#215;</span>
         </div>`
-       
+        supTrip();
+        
+        totalCost += data[i].price
+        console.log(totalCost)
+       // totalCost += document.querySelectorAll('.purchase-price');
     }
+
+    document.querySelector('#total').textContent = totalCost;
   })
 
 
 
 
 
-function supTrip() {
-  let X = document.querySelectorAll('.delete');
-  for (let i = 0; i < X.length; i++) {
-  X[i].addEventListener('click', function() {
-    this.parentNode.remove();
-    compteur();
-  })
-}
-}
-supTrip();
+
+
+
+
